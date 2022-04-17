14890bee7b1b855ae85d642927c91bfc412ea077
JimeRamos <jimejis7@gmail.com>
2022-04-17 18:55:31 -0300

    Creacion de la clase Principal11 del punto11

---
+++ TP2_Grupo25/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/punto11/Principal11.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.pvisual.aplicacion.punto11;
+
+import ar.edu.unju.fi.pvisual.aplicacion.punto05.Muestra;
+
+public class Principal11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Muestra los números del 320 al 160 en forma descendente");
+		Muestra unmostrar = new Muestra();
+		unmostrar.mostrar_repetir(320);
+
+	}
+
+}

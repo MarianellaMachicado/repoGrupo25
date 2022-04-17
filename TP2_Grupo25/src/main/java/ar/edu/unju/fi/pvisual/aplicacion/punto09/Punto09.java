557c5fd095fecb8fe0fc195c150cea08875eaf3d
MarianellaMachicado <machicadomarianella@gmail.com>
2022-04-17 01:09:48 -0300

    Punto 9 - Se agrega la clase Principal09

---
+++ TP2_Grupo25/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/punto09/Punto09.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.pvisual.aplicacion.punto09;
+
+public class Punto09 {
+
+	public static void main(String[] args) {
+		for(int i=320;i>=160;i=i-20)
+			System.out.println(i);
+	}
+
+}

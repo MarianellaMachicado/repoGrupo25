23c2cbbfe8af224cd267c444e8d44f07069118e4
GregorioT72 <gastony.015@gmail.com>
2022-04-18 01:03:26 -0300

    Se creo la clase principal07

---
+++ TP2_Grupo25/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/punto07/Principal07.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.pvisual.aplicacion.punto07;
+
+public class Principal07 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j=41;
+
+        for(int i=39;i>=-39;i=i-2)
+        {
+            j=j + i;
+            System.out.print(j);
+            System.out.print(" ");
+        }
+
+	}
+
+
+}

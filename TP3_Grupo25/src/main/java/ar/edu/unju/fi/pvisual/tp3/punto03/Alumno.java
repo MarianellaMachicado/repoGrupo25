69b2086e80a4f057f635bf7ab3e29e806953fb84
JimeRamos <jimejis7@gmail.com>
2022-04-24 17:40:26 -0300

    Se crea Clases Principal 03 y Alumno. Desarrollo del Punto03

---
+++ TP3_Grupo25/src/main/java/ar/edu/unju/fi/pvisual/tp3/punto03/Alumno.java
@@ -0,0 +1,75 @@
+package ar.edu.unju.fi.pvisual.tp3.punto03;
+
+public class Alumno {
+
+	private String nombre, apellido;
+	private int lu;
+	private double[] notaalu;
+
+	public Alumno() {
+		
+	}
+	
+	Alumno(String nombre, String apellido, int lu, double[] nota) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.lu = lu;
+		this.notaalu = nota;
+	}
+
+	public void mostrar() {
+		for(int i=0; i< notaalu.length;i++ ) {
+			System.out.println(notaalu[i]);
+		}
+	}
+	
+	public void calcularPromedio() {
+		double promedio=0;
+		double totalprom = 0;
+		
+		for(int i=0; i< notaalu.length;i++ ) {
+			//System.out.println(notaalu[i]);
+			promedio = promedio + notaalu[i];
+		}
+		totalprom = promedio/5;
+		System.out.println("El promedio es: " +totalprom);
+		//return promedio;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getLu() {
+		return lu;
+	}
+
+	public void setLu(int lu) {
+		this.lu = lu;
+	}
+
+	public double[] getNota() {
+		return notaalu;
+	}
+
+	public void setNota(double[] nota) {
+		this.notaalu = nota;
+	}
+
+	
+	
+		
+}
---
+++ TP3_Grupo25/src/main/java/ar/edu/unju/fi/pvisual/tp3/punto03/Principal03.java
@@ -0,0 +1,38 @@
+package ar.edu.unju.fi.pvisual.tp3.punto03;
+
+import java.util.Scanner;
+
+public class Principal03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		double Notas[] = new double[5];
+		//Variables				
+		String nom, ape;
+		int lu;
+		//Ingreso de datos
+		System.out.println("   INGRESE DATOS DEL ALUMNO");
+		System.out.print("Nombre: ");
+		nom = entrada.next();
+		System.out.print("Apellido: ");
+		ape = entrada.next();
+		System.out.print("LU: ");
+		lu = entrada.nextInt();		
+		int cant=0;
+		
+		do{
+			System.out.print("Nota: ");
+			Notas[cant] = entrada.nextDouble();	
+			cant++;
+		}while(cant<5);
+		
+		Alumno unalumno = new Alumno(nom,ape,lu,Notas);
+		
+		System.out.println("Nombre y Apellido: "+unalumno.getNombre()+" "+unalumno.getApellido());
+		System.out.println("LU: "+ unalumno.getLu());
+		unalumno.calcularPromedio();
+		entrada.close();
+	}
+
+}

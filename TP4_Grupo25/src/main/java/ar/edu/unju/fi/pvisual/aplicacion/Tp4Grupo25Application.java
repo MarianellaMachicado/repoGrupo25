cecfd628994eb0fb884cc1fc2376df6db73a2c18
MarianellaMachicado <machicadomarianella@gmail.com>
2022-05-01 21:29:47 -0300

    Se crea el proyecto base para el tp4

---
+++ TP4_Grupo25/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/Tp4Grupo25Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo25Application.class, args);
+	}
+
+}
